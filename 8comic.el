e5256d64e80db2c686a0711e48e8d810aafcaf0a
Jen-Chieh <jcs090218@gmail.com>
2020-07-07 14:41:01 +0800

    Update core once.

--- 8comic.el
+++ 8comic.el
@@ -40,14 +40,20 @@
   :group 'tool
   :link '(url-link :tag "Repository" "https://github.com/jcs-elpa/8comic"))
 
-(defconst 8comic--url-html-format "https://comicbus.com/html/%s.html"
+(defconst 8comic--url-base "https://comicbus.com"
+  "URL 8comic base.")
+
+(defconst 8comic--url-html-format (concat 8comic--url-base "/html/%s.html")
   "URL 8comic front page format.")
 
-(defconst 8comic--url-view-1-format "https://comicbus.com/view/%s.html?ch=%s"
+(defconst 8comic--url-view-1-format (concat 8comic--url-base "/view/%s.html?ch=%s")
   "URL 8comic view format for first page.")
 
-(defconst 8comic--url-view-a-format "https://comicbus.com/view/%s.html?ch=%s-%s"
+(defconst 8comic--url-view-a-format (concat 8comic--url-base "/view/%s.html?ch=%s-%s")
   "URL 8comic view format after first page.")
+
+(defvar 8comic--menu-list '()
+  "List of comic menu.")
 
 (defun 8comic--insert-image-by-url (url)
   "Insert image by URL."
@@ -61,23 +67,58 @@
           (insert-image (create-image data nil t)))
       (kill-buffer buffer))))
 
-(defun 8comic--test ()
-  "Test interactive."
-  (interactive)
+(defun 8comic--form-base-url (post-url)
+  "Form full URL by POST-URL."
+  (concat 8comic--url-base post-url))
+
+(defun 8comic--page-404-p (data)
+  "Check if page 404 by html string DATA."
+  (string-match-p "404" data))
+
+(defun 8comic--comic-image (index)
+  "Get the front page image URL by page INDEX."
+  (format (8comic--form-base-url "/pics/0/%s.jpg") index))
+
+(defun 8comic--get-front-page-data (index data)
+  "Get all needed front page data by mange INDEX and html DATA."
+  (unless (8comic--page-404-p data)
+    (message "data: %s" data)))
+
+(defun 8comic--request-page (index)
+  "Requet the comic page by INDEX."
   (request
-    "https://comicbus.com/html/11011.html"
+    (format 8comic--url-html-format index)
     :type "GET"
     :parser 'buffer-string
     :success
     (cl-function
      (lambda (&key data &allow-other-keys)
-       (message "data: %s" data)
-       ))
-    :error
-    (cl-function
-     (lambda (&rest args &key _error-thrown &allow-other-keys)
-       (message "args: %s" args)
-       ))))
+       (8comic--get-front-page-data index data)))))
+
+;;; Table
+
+(defconst 8comic--table-format
+  (vector (list "ID" 8 t)
+          (list "Name" 10 t)
+          (list "Description" 20 t))
+  "Format to assign to `tabulated-list-format' variable.")
+
+(defun 8comic--get-menu-entries ()
+  "Get all the entries for table."
+  (let ((entries '()) (id-count 0))
+    ;; TODO: ..
+    entries))
+
+(define-derived-mode 8comic-menu-mode tabulated-list-mode
+  "8comic-menu-mode"
+  "Major mode for 8comic menu mode."
+  :group '8comic
+  (setq tabulated-list-format 8comic--table-format)
+  (setq tabulated-list-padding 1)
+  (setq tabulated-list-sort-key (cons "Name" nil))
+  (tabulated-list-init-header)
+  (setq tabulated-list-entries (8comic--get-menu-entries))
+  (tabulated-list-print t))
 
 (provide '8comic)
 ;;; 8comic.el ends here
